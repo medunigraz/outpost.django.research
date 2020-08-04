4e93143b2ad39586cbac1be2f2d6295c9a42ec05
Michael Fladischer <michael@fladi.at>
2020-08-04 21:20:51 +0200

    Django 2.1 compatibility.

--- src/outpost/django/research/urls.py
+++ src/outpost/django/research/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import include, url
 
 from . import views
 
+app_name = "research"
+
 urlpatterns = [
     url(r"^search/$", views.SearchView.as_view()),
     url(r"^detail/$", views.DetailView.as_view()),
