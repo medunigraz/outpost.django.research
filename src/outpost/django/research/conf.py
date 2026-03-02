92ef823ecd704add647ee3b9db7e6674b29f9499
Michael Fladischer <michael@fladi.at>
2026-03-02 17:30:13 +0100

    Make FDW integration configurable.

--- src/outpost/django/research/conf.py
+++ src/outpost/django/research/conf.py
@@ -3,6 +3,12 @@ from django.conf import settings
 
 
 class ResearchAppConf(AppConf):
+    FDW_SCHEMA = ""
+    FDW_HOSTNAME = ""
+    FDW_PORT = None
+    FDW_DATABASE = ""
+    FDW_USERNAME = ""
+    FDW_PASSWORD = ""
     DB_MAP = {}
     PUBMED_URL = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils"
     PUBMED_TIMEOUT = 5
