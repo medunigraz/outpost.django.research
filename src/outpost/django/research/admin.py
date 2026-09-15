7689b8aa4b826012991abcc0809a742f806068ae
sk1pzz <lukas.klavzer@gmail.com>
2026-09-15 09:12:52 +0200

    Consolidate research admins under a shared ResearchModelAdmin base class.

--- src/outpost/django/research/admin.py
+++ src/outpost/django/research/admin.py
@@ -1,91 +1,92 @@
 from types import MethodType
-
 from django.contrib import admin
 from django.contrib.admin.views.autocomplete import AutocompleteJsonView
-from django.db.models import OrderBy
 
 from . import models
 
 
-class ResearchAutocompleteJsonView(AutocompleteJsonView):
+class FunderAutocompleteJsonView(AutocompleteJsonView):
     def serialize_result(self, obj, to_field_name):
         result = super().serialize_result(obj, to_field_name)
-
         if isinstance(obj, models.Funder) and self.source_field.name == "parent":
-            details = []
-
-            if obj.street:
-                details.append(obj.street)
-            if obj.city:
-                details.append(obj.city)
-            if obj.country:
-                details.append(str(obj.country))
-
-            result["text"] = obj.name
-
-            if details:
-                result["text"] += f" - {' | '.join(details)}"
-
+            details = [
+                v for v in (obj.street, obj.city, obj.country and str(obj.country)) if v
+            ]
+            result["text"] = obj.name + (f" - {' | '.join(details)}" if details else "")
         return result
 
 
-def custom_autocomplete_view(self, request):
-    return ResearchAutocompleteJsonView.as_view(admin_site=self)(request)
+def _custom_autocomplete_view(self, request):
+    return FunderAutocompleteJsonView.as_view(admin_site=self)(request)
 
 
-admin.site.autocomplete_view = MethodType(
-    custom_autocomplete_view,
-    admin.site,
-)
+admin.site.autocomplete_view = MethodType(_custom_autocomplete_view, admin.site)
 
 
-@admin.register(models.PredominantFunder)
-class PredominantFunderAdmin(admin.ModelAdmin):
+class ResearchModelAdmin(admin.ModelAdmin):
+    list_display = ("id", "__str__")
     search_fields = ("name",)
 
+    def _ensure_id_first(self, fields):
+        fields = list(fields)
+        if "id" in fields:
+            fields.remove("id")
+        return ["id"] + fields
 
-@admin.register(models.LegalBasis)
-class LegalBasisAdmin(admin.ModelAdmin):
-    list_filter = ("active",)
-    search_fields = ("name",)
+    def get_readonly_fields(self, request, obj=None):
+        return self._ensure_id_first(super().get_readonly_fields(request, obj))
+
+    def get_fields(self, request, obj=None):
+        return self._ensure_id_first(super().get_fields(request, obj))
 
 
-@admin.register(models.Field)
-class FieldAdmin(admin.ModelAdmin):
-    search_fields = ("name",)
+@admin.register(models.Bidding)
+class BiddingAdmin(ResearchModelAdmin):
+    list_display = ("id", "title", "url", "start")
+    list_filter = ("running",)
+    search_fields = ("title",)
+    date_hierarchy = "start"
 
 
-@admin.register(models.ResearchType)
-class ResearchTypeAdmin(admin.ModelAdmin):
-    pass
+@admin.register(models.BiddingDeadline)
+class BiddingDeadlineAdmin(ResearchModelAdmin):
+    autocomplete_fields = ("bidding",)
+    list_display = ("id", "bidding", "deadline")
+    search_fields = ("bidding__title", "comment")
+
+
+@admin.register(models.BiddingEndowment)
+class BiddingEndowmentAdmin(ResearchModelAdmin):
+    autocomplete_fields = ("bidding",)
+    list_display = ("id", "bidding", "amount", "currency")
+    search_fields = ("bidding__title", "information")
 
 
 @admin.register(models.Classification)
-class ClassificationAdmin(admin.ModelAdmin):
+class ClassificationAdmin(ResearchModelAdmin):
     autocomplete_fields = ("parent",)
-    search_fields = ("name",)
+    list_display = ("classification_id", "__str__", "level")
 
 
 @admin.register(models.Country)
-class CountryAdmin(admin.ModelAdmin):
+class CountryAdmin(ResearchModelAdmin):
+    list_display = ("id", "__str__", "group")
+    list_filter = ("group",)
     search_fields = ("name", "iso")
 
 
 @admin.register(models.CountryGroup)
-class CountryGroupAdmin(admin.ModelAdmin):
-    list_display = ("pk", "name")
-    search_fields = ("name",)
-    ordering = ("pk",)
+class CountryGroupAdmin(ResearchModelAdmin):
+    pass
 
 
-@admin.register(models.Language)
-class LanguageAdmin(admin.ModelAdmin):
-    list_display = ("__str__", "iso")
-    search_fields = ("name", "iso")
+@admin.register(models.Field)
+class FieldAdmin(ResearchModelAdmin):
+    list_filter = ("active",)
 
 
 @admin.register(models.Funder)
-class FunderAdmin(admin.ModelAdmin):
+class FunderAdmin(ResearchModelAdmin):
     autocomplete_fields = (
         "country",
         "typeintellectualcapitalaccounting",
@@ -93,7 +94,8 @@ class FunderAdmin(admin.ModelAdmin):
         "parent",
     )
     list_display = (
-        "__str__",
+        "id",
+        "name",
         "abbreviation",
         "street",
         "zipcode",
@@ -108,119 +110,108 @@ class FunderAdmin(admin.ModelAdmin):
         "patron_peer_review",
         "typeintellectualcapitalaccounting",
         "typestatisticsaustria",
+        "country",
     )
-    readonly_fields = ("id",)
     search_fields = ("name", "abbreviation")
-
-    def get_fields(self, request, obj=None):
-        fields = list(super().get_fields(request, obj))
-        return ["id", *[field for field in fields if field != "id"]]
-
-
-@admin.register(models.ProjectCategory)
-class ProjectCategoryAdmin(admin.ModelAdmin):
-    list_filter = ("third_party_funding_policy",)
-    search_fields = ("name",)
-
-
-@admin.register(models.ProjectType)
-class ProjectTypeAdmin(admin.ModelAdmin):
-    list_filter = ("public",)
-    search_fields = ("name",)
-
-
-@admin.register(models.ProjectResearch)
-class ProjectResearchAdmin(admin.ModelAdmin):
-    list_filter = ("active",)
-    search_fields = ("name",)
-
-
-@admin.register(models.ProjectFunction)
-class ProjectFunctionAdmin(admin.ModelAdmin):
-    list_filter = ("active",)
-    search_fields = ("name",)
-
-
-@admin.register(models.ProjectPartnerFunction)
-class ProjectPartnerFunctionAdmin(admin.ModelAdmin):
-    search_fields = ("name",)
-
-
-@admin.register(models.ProjectStudy)
-class ProjectStudyAdmin(admin.ModelAdmin):
-    list_filter = ("active",)
-    search_fields = ("name",)
-
-
-@admin.register(models.ProjectEvent)
-class ProjectEventAdmin(admin.ModelAdmin):
-    search_fields = ("name",)
-
-
-@admin.register(models.ProjectGrant)
-class ProjectGrantAdmin(admin.ModelAdmin):
-    search_fields = ("name",)
-
-
-@admin.register(models.Program)
-class ProgramAdmin(admin.ModelAdmin):
-    autocomplete_fields = ("funder",)
-    list_display = ("name", "funder")
-    list_filter = ("active",)
-    search_fields = ("name", "funder__name")
-
-
-@admin.register(models.Bidding)
-class BiddingAdmin(admin.ModelAdmin):
-    list_display = ("title", "url", "start")
-    list_display_links = ("title",)
-    list_filter = ("running",)
-    search_fields = ("title",)
-    date_hierarchy = "start"
-
-
-@admin.register(models.BiddingDeadline)
-class BiddingDeadlineAdmin(admin.ModelAdmin):
-    autocomplete_fields = ("bidding",)
-    list_display = ("bidding", "deadline")
-    list_display_links = ("bidding",)
-    search_fields = ("bidding__title", "comment")
-
-
-@admin.register(models.BiddingEndowment)
-class BiddingEndowmentAdmin(admin.ModelAdmin):
-    autocomplete_fields = ("bidding",)
-    list_display = ("bidding", "amount", "currency")
-    list_display_links = ("bidding",)
-    search_fields = ("bidding__title", "information")
 
 
 @admin.register(models.FunderTypeIntellectualCapitalAccounting)
-class FunderTypeIntellectualCapitalAccountingAdmin(admin.ModelAdmin):
-    search_fields = ("name",)
+class FunderTypeIntellectualCapitalAccountingAdmin(ResearchModelAdmin):
+    pass
 
 
 @admin.register(models.FunderTypeStatisticsAustria)
-class FunderTypeStatisticsAustriaAdmin(admin.ModelAdmin):
-    search_fields = ("name",)
+class FunderTypeStatisticsAustriaAdmin(ResearchModelAdmin):
+    pass
 
 
-@admin.register(models.ServiceProvider)
-class ServiceProviderAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "active")
+@admin.register(models.Language)
+class LanguageAdmin(ResearchModelAdmin):
+    list_display = ("id", "__str__", "iso")
+    search_fields = ("name", "iso")
 
 
-@admin.register(models.ServiceProviderContact)
-class ServiceProviderContactAdmin(admin.ModelAdmin):
-    list_display = ("id", "serviceprovider", "name", "email")
+@admin.register(models.LegalBasis)
+class LegalBasisAdmin(ResearchModelAdmin):
+    list_filter = ("active",)
+
+
+@admin.register(models.PredominantFunder)
+class PredominantFunderAdmin(ResearchModelAdmin):
+    pass
+
+
+@admin.register(models.Program)
+class ProgramAdmin(ResearchModelAdmin):
+    autocomplete_fields = ("funder",)
+    list_display = ("id", "name", "funder")
+    list_filter = ("active", "funder")
+    search_fields = ("name", "funder__name")
+
+
+@admin.register(models.ProjectCategory)
+class ProjectCategoryAdmin(ResearchModelAdmin):
+    list_filter = ("third_party_funding_policy",)
+
+
+@admin.register(models.ProjectEvent)
+class ProjectEventAdmin(ResearchModelAdmin):
+    pass
+
+
+@admin.register(models.ProjectFunction)
+class ProjectFunctionAdmin(ResearchModelAdmin):
+    list_filter = ("active",)
+
+
+@admin.register(models.ProjectGrant)
+class ProjectGrantAdmin(ResearchModelAdmin):
+    pass
 
 
 @admin.register(models.ProjectMentorContribution)
-class ProjectMentorContributionAdmin(admin.ModelAdmin):
+class ProjectMentorContributionAdmin(ResearchModelAdmin):
     list_filter = ("active",)
-    search_fields = ("name",)
+
+
+@admin.register(models.ProjectPartnerFunction)
+class ProjectPartnerFunctionAdmin(ResearchModelAdmin):
+    pass
+
+
+@admin.register(models.ProjectResearch)
+class ProjectResearchAdmin(ResearchModelAdmin):
+    list_filter = ("active",)
+
+
+@admin.register(models.ProjectStudy)
+class ProjectStudyAdmin(ResearchModelAdmin):
+    list_filter = ("active",)
+
+
+@admin.register(models.ProjectType)
+class ProjectTypeAdmin(ResearchModelAdmin):
+    list_filter = ("public",)
+
+
+@admin.register(models.ResearchType)
+class ResearchTypeAdmin(ResearchModelAdmin):
+    pass
+
+
+@admin.register(models.ServiceProvider)
+class ServiceProviderAdmin(ResearchModelAdmin):
+    list_display = ("id", "__str__", "active")
+    search_fields = ("campusonline",)
+
+
+@admin.register(models.ServiceProviderContact)
+class ServiceProviderContactAdmin(ResearchModelAdmin):
+    autocomplete_fields = ("serviceprovider", "campusonline")
+    list_display = ("id", "serviceprovider", "name", "email")
+    list_filter = ("serviceprovider",)
 
 
 @admin.register(models.Sponsorship)
-class SponsorshipAdmin(admin.ModelAdmin):
+class SponsorshipAdmin(ResearchModelAdmin):
     list_display = ("id", "__str__", "active")
